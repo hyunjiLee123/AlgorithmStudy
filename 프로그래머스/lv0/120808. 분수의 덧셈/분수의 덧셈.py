00684c52531d342dff432365b51085c99147a582
hyunjiLee <79522198+hyunjiLee123@users.noreply.github.com>
2023-04-30 17:45:58 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.38 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -0,0 +1,10 @@
+def solution(numer1, denom1, numer2, denom2):
+    answer=[0,0]
+    answer[1] = denom1*denom2
+    answer[0] = numer1*denom2 + numer2*denom1
+    for i in range(min(answer[0], answer[1]), 1, -1):
+        if (answer[0]%i==0 and answer[1]%i==0):
+            answer[0] //= i
+            answer[1] //= i
+    
+    return answer
